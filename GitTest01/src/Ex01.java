e9c25a0d47f67f437aec915e64712106735dd543
User <User@192.168.219.195>
2024-01-30 15:48:49 +0900

    팀원1 두번째 커밋!

--- GitTest01/src/Ex01.java
+++ GitTest01/src/Ex01.java
@@ -2,8 +2,8 @@
 public class Ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("첫번째 커밋!");
+		System.out.println("팀원1 두번째 커밋!");
 	}
 
 }
