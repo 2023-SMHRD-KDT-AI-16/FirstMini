34f66bc09102baf557daea9c6066c2213f039f10
User <User@192.168.219.185>
2024-01-31 17:11:27 +0900

    회원가입DAO DTO MAIN커밋

---
+++ GitTest01/src/Model/main.java
@@ -0,0 +1,82 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import Controller.memberDAO;
+import Model.memberDTO;
+
+public class main {
+
+	public static void main(String[] args) {
+
+		try (Scanner sc = new Scanner(System.in)) {
+			memberDAO mdao = new memberDAO();
+			// ArrayList<memberDTO> dtoList = new ArrayList<memberDTO>();
+
+			while (true) {
+				System.out.println("1.회원가입 2.로그인 3.회원탈퇴 6.종료");
+
+				int choice = sc.nextInt();
+				if (choice == 6) {
+					System.out.println("종료합니다.");
+					break;
+
+					// =======회원가입
+				} else if (choice == 1) {
+					System.out.println("=== 회원가입 ===");
+					System.out.print("사용할 아이디를 입력하세요: ");
+					String join_id = sc.next();
+					if (mdao.id_Check(join_id) == true) { // 중복id확인
+						System.out.println("중복된 ID입니다. 다시입력해주세요.");
+						break;
+					}
+
+					System.out.print("비밀번호를 입력하세요: ");
+					String join_pw = sc.next();
+
+					System.out.print("이름을 입력하세요: ");
+					String join_name = sc.next();
+
+					memberDTO mdto = new memberDTO(join_id, join_pw, join_name);
+					int row = mdao.joinMember(mdto);
+
+					if (row > 0) {
+						System.out.println("회원가입을 축하합니다!");
+					} else {
+						System.out.println("insert fail");
+					}
+
+					// ========로그인
+				} else if (choice == 2) {
+					System.out.println("===로그인===");
+					System.out.print(" 아이디: ");
+					String log_id = sc.next();
+					System.out.print("비밀번호: ");
+					String log_pw = sc.next();
+
+					mdao = new memberDAO();
+					memberDTO mdto = mdao.login(log_id, log_pw);
+					if (mdto != null) {
+						System.out.println("로그인 되었습니다.");
+					} else {
+						System.out.println("잘못 입력 했습니다.");
+					}
+					// ========탈퇴
+				} else if (choice == 3) {
+
+					System.out.print("탈퇴할 아이디를 입력하세요: ");
+					String delete_id = sc.next();
+
+					mdao = new memberDAO();
+					int row = mdao.delete(delete_id);
+
+					if (row > 0) {
+						System.out.println("delete success");
+					} else {
+						System.out.println("delete fail");
+					}
+				}
+			}
+		}
+	}
+}
---
+++ GitTest01/src/Model/memberDAO.java
@@ -0,0 +1,143 @@
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import Model.memberDTO;
+
+public class memberDAO {
+
+	private Connection conn = null;
+	private PreparedStatement psmt = null;
+	private ResultSet rs = null;
+
+	// connection
+	private void getConn() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String user = "mp_21K_bigdata22_p1_5";
+			String pw = "smhrd5";
+			String url = "jdbc:oracle:thin:@project-db-campus.smhrd.com:1523:xe";
+
+			conn = DriverManager.getConnection(url, user, pw);
+//         if (conn != null) {
+//				System.out.println("연결 성공");
+//			} else {
+//				System.out.println("연결 실패");
+//			}
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+
+		}
+	}
+
+	// close 하는 메소드
+	private void allClose() {
+		try {
+			if (rs != null)
+				rs.close();
+			if (psmt != null)
+				psmt.close();
+			if (conn != null)
+				conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// id중복
+	public boolean id_Check(String id) {// 회원가입할 때 입력된 id
+
+		getConn();
+		String sql = "SELECT COUNT(ID) FROM MEMBER WHERE ID=?";
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, id);
+			rs = psmt.executeQuery();
+
+			rs.next();
+			if (rs.getInt(1) == 1) {
+				return true;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			allClose();
+		}
+		return false;
+	}
+
+	// 회원가입
+	public int joinMember(memberDTO mdto) {
+		getConn();
+		String sql = "insert into member(id, pw, name) values( ?, ?, ? )";
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, mdto.getId());
+			psmt.setString(2, mdto.getPw());
+			psmt.setString(3, mdto.getName());
+
+			int row = psmt.executeUpdate();
+
+			return row;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return 0;
+		} finally {
+			allClose();
+		}
+	}
+
+	// 로그인
+	public memberDTO login(String log_id, String log_pw) {
+		// ArrayList<memberDTO> dtoList = new ArrayList<memberDTO>();
+
+		getConn();
+		memberDTO mdto = null;
+		String sql = "select *  from member where id=? and pw=?";
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, log_id);
+			psmt.setString(2, log_pw);
+
+			rs = psmt.executeQuery();
+
+			if (rs.next()) {
+				String id = rs.getString(1);
+				String pw = rs.getString(2);
+				String name = rs.getString(3);
+				mdto = new memberDTO(id, pw, name);
+			}
+			return mdto;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	// 탈퇴
+	public int delete(String del_id) {
+		getConn();
+		String sql = "delete from member where id=?";
+		try {
+			psmt = conn.prepareStatement(sql);
+			psmt.setString(1, del_id);
+			int row = psmt.executeUpdate();
+
+			return row;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return 0;
+		} finally {
+			allClose();
+		}
+	}
+
+}
